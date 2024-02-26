a03c3f5cc9299ea1eec67f9f62f2c26ba9acd9b9
Bryan Durushia <b.durushia@gmail.com>
2024-02-26 14:38:55 -0600

    Created Car class and properties. Made new instance of Car class and set values. Made a method to print info to console. Exercise complete.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Creating a new instance of the Car class
+            var myCar = new Car();
+
+            // Set values in the properties for the object
+            myCar.Year = 2012;
+            myCar.Make = "Volkswagen";
+            myCar.Model = "Golf TDI";
+            myCar.Transmission = "6-Speed Manual";
+
+            // Print the values of each property to the Console
+            myCar.PrintCar();
         }
     }
 }
